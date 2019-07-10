62fd6de586a748a43c2f0b1998eee1d3aa917417
ru.dev13 <ru.dev13@trezub.ru>
2019-07-10 16:56:18 +0300

    faq block is ready

---
+++ assets/template/kgk/js/main.js
@@ -0,0 +1,16 @@
+// Accordion for faq section
+
+var acc = document.getElementsByClassName("section-faq__accordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+    acc[i].onclick = function() {
+        this.classList.toggle("active");
+        var panel = this.nextElementSibling;
+        if (panel.style.maxHeight) {
+            panel.style.maxHeight = null;
+        } else {
+            panel.style.maxHeight = panel.scrollHeight + "px";
+        }
+    }
+}
