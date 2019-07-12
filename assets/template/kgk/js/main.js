bd5f451115f1bb9e61b222aae28b921525d99c02
ru.dev13 <ru.dev13@trezub.ru>
2019-07-12 15:19:32 +0300

    clients slider is ready

--- assets/template/kgk/js/main.js
+++ assets/template/kgk/js/main.js
@@ -20,6 +20,42 @@ for (i = 0; i < acc.length; i++) {
 }
 
 
+// Slick slider for 'clients' section
+$(document).ready(function() {
+    $('.section-clients__items').slick({
+        dots: false,
+        autoplay: true,
+        autoplaySpeed: 4500,
+        infinite: true,
+        speed: 500,
+        slidesToShow: 4,
+        slidesToScroll: 1,
+        responsive: [{
+                breakpoint: 1024,
+                settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 1,
+                }
+            },
+            {
+                breakpoint: 600,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 1
+                }
+            },
+            {
+                breakpoint: 480,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1
+                }
+            }
+        ]
+    });
+});
+
+
 // Slick slider for 'functions' section
 $(document).ready(function() {
     $('.section-functions__items').slick({
